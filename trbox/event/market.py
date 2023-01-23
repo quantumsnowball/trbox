b7a76410d22bc38f1bc8e3f04821e9e6d754b79b
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-24 00:13:11 +0800

    event: define ohlcv request/response event

--- trbox/event/market.py
+++ trbox/event/market.py
@@ -1,11 +1,29 @@
+from dataclasses import dataclass
+from trbox.common.types import Symbol
 from trbox.event import MarketEvent
+from pandas import DataFrame
 
 
 #
+# Requests
+#
+@dataclass
+class OhlcvWindowRequest(MarketEvent):
+    symbol: Symbol
+    length: int
+
+
 class PriceFeedRequest(MarketEvent):
     def __init__(self,
                  symbol: Symbol):
         self._symbol = symbol
+
+
+#
+# Response
+#
+class OhlcvWindow(MarketEvent):
+    dfs: dict[Symbol, DataFrame]
 
 
 class Price(MarketEvent):
