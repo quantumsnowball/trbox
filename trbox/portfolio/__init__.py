3b0e4f48dd2197735824320a9d19347e3730118b
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-08 20:20:00 +0800

    rft: create account status fields

--- trbox/portfolio/__init__.py
+++ trbox/portfolio/__init__.py
@@ -2,6 +2,7 @@ from abc import ABC
 
 from typing_extensions import override
 
+from trbox.common.types import Symbol
 from trbox.event import Event
 from trbox.event.handler import CounterParty
 
@@ -9,6 +10,20 @@ from trbox.event.handler import CounterParty
 class Portfolio(CounterParty, ABC):
     def __init__(self) -> None:
         super().__init__()
+
+    # account status
+
+    @property
+    def cash(self) -> float:
+        return self._broker.cash
+
+    @property
+    def positions(self) -> dict[Symbol, float]:
+        return self._broker.positions
+
+    @property
+    def equity(self) -> float:
+        return self._broker.equity
 
 
 class DefaultPortfolio(Portfolio):
