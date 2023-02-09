0265954333625771fe52b901a94ec594348e5c72
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-09 21:49:22 +0800

    feat: should only reply to the sender

--- tests/lab/websocket/socket-chat.py
+++ tests/lab/websocket/socket-chat.py
@@ -17,7 +17,8 @@ def index():
 @socketio.on('message')
 def handle_message(message):
     print(f'received message: {message}')
-    send(message, broadcast=True)
+    # send(message, broadcast=True)
+    send(message)
 
 
 if __name__ == '__main__':
