2c607862c25a1a75b956aa4922562edb70e38310
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-11 17:25:28 +0800

    feat: running website on 5000 and websocket on 8000, each with its own thread

--- trbox/console/aiohttp_ws.py
+++ trbox/console/aiohttp_ws.py
@@ -1,9 +1,9 @@
 from threading import Thread
 
-import aiohttp
 from aiohttp import web
 from socketio.asyncio_client import asyncio
 from typing_extensions import override
+from websockets.server import serve
 
 from trbox.common.logger import Log
 from trbox.common.logger.parser import Memo
@@ -25,41 +25,21 @@ routes = web.RouteTableDef()
 async def index(_):
     return web.FileResponse(ENTRY_POINT)
 
-
-@routes.get('/ws')
-async def websocket_handler(request):
-    Log.critical('hey ws, do you copy?')
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    async for msg in ws:
-        # sample msg: WSMessage(type=<WSMsgType.TEXT: 1>, data='1676103725066', extra='')
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            if msg.data == 'close':
-                await ws.close()
-            else:
-                await ws.send_str(msg.data + '/answer')
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            Log.critical('ws connection closed with exception %s' %
-                         ws.exception())
-
-    Log.critical('websocket connection closed')
-
-    return ws
-
-
 routes.static('/', FRONTEND_LOCAL_DIR)
+
 
 #
 # websocket
 #
+async def echo(websocket):
+    async for message in websocket:
+        Log.critical(message)
+        await websocket.send(message)
 
 
 #
 # error handling
 #
-
-
 @web.middleware
 async def on_request_error(request: web.Request, handler):
     try:
@@ -76,27 +56,41 @@ async def on_request_error(request: web.Request, handler):
 
 class AioHttpServer(Console):
     def __init__(self,
-                 port: int = 5000) -> None:
+                 port_website: int = 5000,
+                 port_websocket: int = 8000) -> None:
         super().__init__()
-        self._port = port
+        self._port_website = port_website
+        self._port_websocket = port_websocket
         self._app = web.Application(middlewares=[on_request_error, ])
         self._app.add_routes(routes)
         self._runner = web.AppRunner(self._app)
 
-    def run_server(self):
-        async def run():
+    def run_services(self):
+        async def websocket():
+            async with serve(echo, port=self._port_websocket):
+                await asyncio.Future()  # run forever
+
+        async def website():
             await self._runner.setup()
-            site = web.TCPSite(self._runner, port=self._port)
+            site = web.TCPSite(self._runner, port=self._port_website)
             await site.start()
-            while True:
-                await asyncio.sleep(3600)  # sleep forever
+            await asyncio.Future()  # run forever
+
         # asyncio loop run in its own thread
-        server_thread = Thread(target=asyncio.run, args=(run(), ), daemon=True)
-        server_thread.start()
+        for service in (website(),
+                        websocket(), ):
+            t = Thread(target=asyncio.run,
+                       args=(service, ),
+                       daemon=True)
+            t.start()
+
+        Log.info(Memo(f'Starting website on port {self._port_website}'
+                      f' and websocket on port {self._port_websocket}')
+                 .by(self))
 
     @override
     def start(self) -> None:
-        self.run_server()
+        self.run_services()
 
     @override
     def stop(self) -> None:
