0a272404827777a44231ed5fcb7061ca2b83379a
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-16 18:31:11 +0800

    test: cagr

--- tests/portfolio/test_metrics.py
+++ tests/portfolio/test_metrics.py
@@ -1,7 +1,7 @@
 import pytest
 from pandas import Series, Timedelta, Timestamp
 
-from trbox.portfolio.metrics import total_return
+from trbox.portfolio.metrics import cagr, total_return
 
 
 def int_ls(start, n):
@@ -20,10 +20,11 @@ def datetime_ls(start, n):
     return [Timestamp(2015, 1, 1) + Timedelta(days=i)
             for i in range(start, start+n)]
 
-
 #
 # tests
 #
+
+
 def test_detect_annualize_factor():
     assert False
 
@@ -41,8 +42,20 @@ def test_total_return(data_ls, index_ls, start, n):
     assert ans == data[-1]/data[0]-1
 
 
-def test_cagr():
-    assert False
+@pytest.mark.parametrize('data_ls', [int_ls, float_ls, ])
+@pytest.mark.parametrize('index_ls', [int_ls, float_ls, str_ls, datetime_ls])
+@pytest.mark.parametrize('start', [10, 100, 1000])
+@pytest.mark.parametrize('n', [10, 100, 1000])
+def test_cagr(data_ls, index_ls, start, n):
+    data = data_ls(start, n)
+    index = index_ls(start, n)
+    series = Series(data, index=index)
+    if index_ls == datetime_ls:
+        ans = cagr(series)
+        assert isinstance(ans, float)
+    else:
+        with pytest.raises(Exception):
+            cagr(series)
 
 
 def test_mu_sigma():
