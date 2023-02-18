18969b2b736a47d1902954a0dfa969fb631df91d
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-18 16:47:27 +0800

    feat: success serving of the Lab site under submodule out/

--- trbox/backtest/lab.py
+++ trbox/backtest/lab.py
@@ -1,18 +1,24 @@
-import glob
 import os
-from asyncio import Future, new_event_loop
+from asyncio import Future
 from threading import Thread
-from typing import Any, Generator, Union
+from typing import Any, Union
 
 import click
 from aiohttp import web
+from aiohttp.typedefs import Handler
 from binance.websocket.binance_socket_manager import json
 from socketio.asyncio_client import asyncio
 from typing_extensions import override
 
+from trbox.common.logger import Log
+from trbox.common.logger.parser import Memo
+
 DEFAULT_HOST = 'localhost'
-DEFAULT_PORT = 9000
+DEFAULT_PORT = 7000
 DEFAULT_PATH = '.'
+FRONTEND_LOCAL_DIR = f'{os.path.dirname(__file__)}/../frontend/trbox-lab/out/'
+DEFAULT_FILENAME = 'index.html'
+ENTRY_POINT = f'{FRONTEND_LOCAL_DIR}{DEFAULT_FILENAME}'
 
 routes = web.RouteTableDef()
 
@@ -48,20 +54,43 @@ class Lab(Thread):
         self._path = path
         self._host = host
         self._port = port
-        self._app = web.Application()
+        self._app = web.Application(middlewares=[self.on_request_error, ])
         self._app.add_routes([
-            web.route('GET', '/', self.lsdir),
+            web.route('GET', '/', self.index),
+            web.static('/', FRONTEND_LOCAL_DIR),
+            web.route('GET', '/tree', self.lsdir),
         ])
         self._runner = web.AppRunner(self._app)
 
     #
     # routes
     #
+    async def index(self, _: web.Request) -> web.FileResponse:
+        return web.FileResponse(ENTRY_POINT)
+
     async def lsdir(self, _) -> web.Response:
-        paths = [dict(name=m.name, path=m.path)
-                 for m in scan_for_st_recursive(self._path)]
-        return web.json_response(paths,
+        tree = scan_for_st_recursive(self._path)
+        return web.json_response(tree,
                                  dumps=lambda s: json.dumps(s, indent=4))
+
+    #
+    # error handling
+    #
+
+    @web.middleware
+    async def on_request_error(self,
+                               request: web.Request,
+                               handler: Handler) -> web.StreamResponse:
+        try:
+            return await handler(request)
+        except web.HTTPNotFound as e404:
+            # FileNotFoundError
+            Log.critical(Memo(e404).by('Middleware'))
+            return web.FileResponse(ENTRY_POINT)
+        except Exception as e:
+            # any other errors
+            Log.exception(e)
+            return web.FileResponse(ENTRY_POINT)
 
     # TODO: propose definition of a `lab` dir:
     # 1. contains a single st_*.py file
@@ -73,6 +102,7 @@ class Lab(Thread):
     #
     # main loop
     #
+
     async def serve(self) -> None:
         click.echo(f'Serving {self._path} at http://{self._host}:{self._port}')
         await self._runner.setup()
