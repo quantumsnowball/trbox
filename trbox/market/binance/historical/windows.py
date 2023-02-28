c7457ab3b112beb2fc168b5d6cbad601d7c6213b
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-28 13:15:21 +0800

    step: gen archive url

--- trbox/market/binance/historical/windows.py
+++ trbox/market/binance/historical/windows.py
@@ -11,28 +11,61 @@ from typing_extensions import override
 from trbox.common.types import Symbols
 from trbox.market import MarketWorker
 
-BASE_ENDPOINT = 'https://api.binance.com'
+API_BASE = 'https://api.binance.com'
+ARCHIVE_BASE = 'https://data.binance.vision/?prefix=data'
 
+MarketType = Literal[
+    'spot',
+    # 'futures', # not implemented yet
+]
+UpdateFreq = Literal[
+    'daily',
+    # 'monthly', # not implemented yet
+]
+DataType = Literal[
+    # 'aggTrades', # not implemented yet
+    'klines',
+    # 'trades', # not implemented yet
+]
 Freq = Literal['1s',
                '1m', '3m', '5m', '15m', '30m',
                '1h', '2h', '4h', '6h', '8h', '12h',
                '1d', ]
 
 
-async def fetch_zip(urls: list[str]) -> DataFrame:
-    async def fetch(url: str,
-                    session: aiohttp.ClientSession) -> DataFrame:
-        async with session.get(url) as res:
-            zipped = ZipFile(BytesIO(await res.content.read()))
-            csv = zipped.open()
-            segment = read_csv(csv)
-            return segment
-
-    async with aiohttp.ClientSession() as session:
-        segments: list[DataFrame] = await asyncio.gather(*[fetch(url, session)
-                                                           for url in urls])
-        df = concat(segments)
-        return df
+async def fetch_zip(symbol: str,
+                    freq: Freq,
+                    start: str,
+                    end: str,
+                    *,
+                    market_type: MarketType = 'spot',
+                    update_freq: UpdateFreq = 'daily',
+                    data_type: DataType = 'klines',):
+    def make_url(date: str) -> str:
+        date = str(to_datetime(date).date())
+        path = f'{API_BASE}/{market_type}/{update_freq}/{data_type}/{symbol}/{freq}'
+        fname = f'{symbol}-{freq}-{date}.zip'
+        url = f'{path}/{fname}'
+        return url
+    url = make_url(start)
+    print()
+    # params = dict(symbol=symbol,
+    #               interval=freq,
+    #               startTime=int(to_datetime(start).value/1e6),
+    #               endTime=int(to_datetime(end).value/1e6),)
+    # async def fetch(url: str,
+    #                 session: aiohttp.ClientSession) -> DataFrame:
+    #     async with session.get(url) as res:
+    #         zipped = ZipFile(BytesIO(await res.content.read()))
+    #         csv = zipped.open()
+    #         segment = read_csv(csv)
+    #         return segment
+    #
+    # async with aiohttp.ClientSession() as session:
+    #     segments: list[DataFrame] = await asyncio.gather(*[fetch(url, session)
+    #                                                        for url in urls])
+    #     df = concat(segments)
+    #     return df
 
 
 async def fetch_api(symbol: str,
@@ -46,7 +79,7 @@ async def fetch_api(symbol: str,
     only useful when fetch some short history data just before live trading to init the startegy
     '''
     path = '/api/v3/klines'
-    url = f'{BASE_ENDPOINT}{path}'
+    url = f'{API_BASE}{path}'
     params = dict(symbol=symbol,
                   interval=freq,
                   startTime=int(to_datetime(start).value/1e6),
@@ -85,6 +118,5 @@ class BinanceHistoricalWindows(MarketWorker):
 
 if __name__ == '__main__':
     async def main():
-        df = await fetch_api('BTCUSDT', '1s', '2023-01-01', '2023-01-10')
-        print(df)
+        await fetch_zip('BTCUSDT', '1h', '2023-01-01', '2023-01-10')
     asyncio.run(main())
