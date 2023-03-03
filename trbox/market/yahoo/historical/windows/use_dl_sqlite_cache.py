1a2fde863210bab239aba10411f69636f7c6592b
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 02:07:27 +0800

    step: create table if not exists

--- trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
+++ trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
@@ -1,7 +1,11 @@
 
-from pandas import DataFrame, Timestamp
+from pathlib import Path
 
-from trbox.market.yahoo.historical.windows.constants import Freq
+import aiosqlite
+from pandas import DataFrame, Timestamp, date_range, to_datetime
+from socketio.asyncio_client import asyncio
+
+from trbox.market.yahoo.historical.windows.constants import CACHE_DIR, Freq
 
 
 async def fetch_sqlite(symbol: str,
@@ -12,5 +16,30 @@ async def fetch_sqlite(symbol: str,
                        retry: int = 10,
                        timeout: int = 5) -> DataFrame:
     # create db if not exist
+    start_: Timestamp = to_datetime(start)
+    end_: Timestamp = to_datetime(end if end else Timestamp.now())
+    dates = [str(d.date())
+             for d in date_range(start, end, freq='D')]
+    cache_dir = f'{CACHE_DIR}/yahoo/historical/windows/{symbol}/{freq}'
+    cache_url = f'{cache_dir}/db.sqlite'
+    Path(cache_dir).mkdir(parents=True, exist_ok=True)
+    async with aiosqlite.connect(cache_url) as db:
+        # assert the table exists
+        sql_create_table = ('CREATE TABLE IF NOT EXISTS ohlcv('
+                            'Open NUMERIC, '
+                            'High NUMERIC, '
+                            'Low NUMERIC, '
+                            'Close NUMERIC, '
+                            'Volume NUMERIC, '
+                            'Date TEXT PRIMARY KEY)')
+        await db.execute(sql_create_table)
+        #
+        tmp = await db.execute_fetchall('PRAGMA table_info(ohlcv);')
+        print(tmp)
 
-    return DataFrame()
+
+if __name__ == '__main__':
+    async def main() -> None:
+        df = await fetch_sqlite('BTC-USD', '1d', '2023-01-15', '2023-01-31')
+        print(df)
+    asyncio.run(main())
