a0426542b1ea122c477fd2d591396bb0d666ab78
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 12:11:48 +0800

    step: use Timestamp as primary key

--- trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
+++ trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
@@ -20,7 +20,7 @@ async def fetch_sqlite(symbol: str,
     # create db if not exist
     start_: Timestamp = to_datetime(start)
     end_: Timestamp = to_datetime(end if end else Timestamp.now())
-    dates = [str(d.date())
+    dates = [d.timestamp()
              for d in date_range(start, end, freq='D')]
     cache_dir = f'{CACHE_DIR}/yahoo/historical/windows/{symbol}/{freq}'
     cache_url = f'{cache_dir}/db.sqlite'
@@ -28,25 +28,22 @@ async def fetch_sqlite(symbol: str,
     async with aiosqlite.connect(cache_url) as db:
         # assert the table exists
         sql_create_table = ('CREATE TABLE IF NOT EXISTS ohlcv('
-                            'Date TEXT PRIMARY KEY, '
+                            'Timestamp INT PRIMARY KEY, '
                             'Open NUMERIC, '
                             'High NUMERIC, '
                             'Low NUMERIC, '
                             'Close NUMERIC, '
                             'Volume NUMERIC)')
         await db.execute(sql_create_table)
-        #
-        tmp = await db.execute_fetchall('PRAGMA table_info(ohlcv);')
-        print(tmp)
         # download and cache the missing period
-        sources = [str(to_datetime(r[0]).date())
-                   for r in await db.execute_fetchall('SELECT Date FROM ohlcv')]
+        sources = [ts
+                   for ts in await db.execute_fetchall('SELECT Timestamp FROM ohlcv')]
         missing = set(dates).difference(set(sources))
         # download and cache the missing source
         if len(missing) > 0:
-            missing_start = min(missing)
-            missing_end = str(to_datetime(
-                max(missing)).date() + Timedelta(days=1))
+            missing_start = str(to_datetime(min(missing), unit='s').date())
+            missing_end = str((to_datetime(max(missing), unit='s') +
+                               Timedelta(days=1)).date())
 
             def download(symbol: str) -> DataFrame:
                 ticker = yf.Ticker(symbol)
@@ -60,13 +57,14 @@ async def fetch_sqlite(symbol: str,
             print(f'downloaded ohlcv, symbol="{symbol}", shape={df.shape}',
                   flush=True)
             df_tuples = list(df.itertuples(index=True))
-            entries = [tuple((t[0].isoformat(), *t[1:])) for t in df_tuples]
+            entries = [tuple((int(t[0].timestamp()), *t[1:]))
+                       for t in df_tuples]
             await db.executemany('REPLACE INTO ohlcv VALUES(?, ?, ?, ?, ?, ?)', entries)
             await db.commit()
         # read the requested data
-        sql_select = ('SELECT Date,Open,High,Low,Close,Volume '
+        sql_select = ('SELECT Timestamp,Open,High,Low,Close,Volume '
                       'FROM ohlcv WHERE '
-                      f"Date BETWEEN '{start_.isoformat()}' AND '{end_.isoformat()}'")
+                      f"Timestamp BETWEEN {start_.timestamp()} AND {end_.timestamp()}")
         data = await db.execute_fetchall(sql_select)
         print(data)
 
