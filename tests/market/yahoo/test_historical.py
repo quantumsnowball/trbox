91d61d489d96126eb6c1cf027e8b63af3e47eb0e
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 13:40:34 +0800

    test: pass

---
+++ tests/market/yahoo/test_historical.py
@@ -0,0 +1,22 @@
+import asyncio
+
+import pytest
+from pandas import DataFrame, to_datetime
+
+from trbox.market.yahoo.historical.windows.use_dl_sqlite_cache import \
+    fetch_sqlite
+
+
+@pytest.mark.parametrize('symbol', ['BTC-USD', 'ETH-USD', 'BNB-USD'])
+@pytest.mark.parametrize('freq', ['1d', ])
+@pytest.mark.parametrize('start', ['2022-01-01', '2022-01-31',])
+@pytest.mark.parametrize('end', ['2023-01-01', '2023-01-31',])
+def test_fetch_sqlite(symbol, freq, start, end):
+    async def main():
+        df = await fetch_sqlite(symbol, freq, start, end)
+        assert isinstance(df, DataFrame)
+        assert df.shape[1] == 5
+        assert df.index.is_monotonic_increasing
+        assert df.index[0] >= to_datetime(start)
+        assert df.index[-1] <= to_datetime(end)
+    asyncio.run(main())
