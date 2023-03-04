493a4dd99c084905355838fa2a650a2617edfbd7
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 13:39:47 +0800

    fix: don't use yfinance, failed at multi threads, download with raw url

--- trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
+++ trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
@@ -1,12 +1,13 @@
-
+from io import BytesIO
 from pathlib import Path
 
+import aiohttp
 import aiosqlite
-import yfinance as yf
-from pandas import DataFrame, Timedelta, Timestamp, date_range, to_datetime
+from pandas import (DataFrame, Timestamp, date_range, read_csv,
+                    to_datetime)
 from socketio.asyncio_client import asyncio
 
-from trbox.common.constants import OHLCV_COLUMN_NAMES, OHLCV_INDEX_NAME
+from trbox.common.constants import OHLCV_INDEX_NAME
 from trbox.market.yahoo.historical.windows.constants import CACHE_DIR, Freq
 
 
@@ -36,31 +37,36 @@ async def fetch_sqlite(symbol: str,
                             'Volume NUMERIC)')
         await db.execute(sql_create_table)
         # download and cache the missing period
-        sources = [ts
-                   for ts in await db.execute_fetchall('SELECT Timestamp FROM ohlcv')]
-        missing = set(dates).difference(set(sources))
+        exists = [ts[0]
+                  for ts in await db.execute_fetchall('SELECT Timestamp FROM ohlcv')]
+        missing = set(dates).difference(set(exists))
         # download and cache the missing source
         if len(missing) > 0:
-            missing_start = str(to_datetime(min(missing), unit='s').date())
-            missing_end = str((to_datetime(max(missing), unit='s') +
-                               Timedelta(days=1)).date())
+            async with aiohttp.ClientSession() as session:
+                missing_start = int(min(missing))
+                missing_end = int(max(missing)) + 60*60*24
 
-            def download(symbol: str) -> DataFrame:
-                ticker = yf.Ticker(symbol)
-                df: DataFrame = ticker.history(start=missing_start,
-                                               end=missing_end,
-                                               interval=freq)
-                df = DataFrame(df.tz_localize(None))
-                df = df[OHLCV_COLUMN_NAMES]
-                return df
-            downloaded = await asyncio.to_thread(download, symbol)
-            print(f'downloaded ohlcv, symbol="{symbol}", shape={downloaded.shape}',
-                  flush=True)
-            downloaded_tuples = list(downloaded.itertuples(index=True))
-            entries = [tuple((int(t[0].timestamp()), *t[1:]))
-                       for t in downloaded_tuples]
-            await db.executemany('REPLACE INTO ohlcv VALUES(?, ?, ?, ?, ?, ?)', entries)
-            await db.commit()
+                async def download() -> DataFrame:
+                    base = 'https://query1.finance.yahoo.com/v7/finance/download'
+                    other_params = 'interval=1d&events=history&includeAdjustedClose=true'
+                    download_url = f'{base}/{symbol}?period1={missing_start}&period2={missing_end}&{other_params}'
+                    async with session.get(download_url) as res:
+                        assert res.status == 200
+                        csv = await res.content.read()
+                        df = read_csv(BytesIO(csv))
+                        df = df.set_index('Date')
+                        df.index = to_datetime(df.index.values)
+                        df = df.drop('Close', axis=1)
+                        df = df.rename(columns={'Adj Close': 'Close'})
+                        return df
+                downloaded = await download()
+                print(f'downloaded ohlcv, symbol="{symbol}", shape={downloaded.shape}',
+                      flush=True)
+                downloaded_tuples = list(downloaded.itertuples(index=True))
+                entries = [tuple((int(t[0].timestamp()), *t[1:]))
+                           for t in downloaded_tuples]
+                await db.executemany('REPLACE INTO ohlcv VALUES(?, ?, ?, ?, ?, ?)', entries)
+                await db.commit()
         # read the requested data
         sql_select = ('SELECT Timestamp,Open,High,Low,Close,Volume '
                       'FROM ohlcv WHERE '
@@ -79,6 +85,6 @@ async def fetch_sqlite(symbol: str,
 
 if __name__ == '__main__':
     async def main() -> None:
-        df = await fetch_sqlite('BTC-USD', '1d', '2022-01-15', '2023-01-31')
+        df = await fetch_sqlite('BTC-USD', '1d', '2021-01-15', '2023-01-31')
         print(df)
     asyncio.run(main())
