7aecd64bc8bc198e6c16c24fb1db5f140fa61d1e
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 14:34:05 +0800

    test: pass

--- tests/market/yahoo/test_historical.py
+++ tests/market/yahoo/test_historical.py
@@ -1,4 +1,5 @@
 import asyncio
+import itertools
 
 import pytest
 from pandas import DataFrame, to_datetime
@@ -6,20 +7,33 @@ from pandas import DataFrame, to_datetime
 from trbox.market.yahoo.historical.windows.use_dl_sqlite_cache import \
     fetch_sqlite
 
+FREQ = '1d'
+CRYPTOS = itertools.product(
+    ['BTC-USD', 'ETH-USD', 'BNB-USD'],
+    ['2022-01-01', '2022-01-31',],
+    ['2023-01-01', '2023-01-31',],
+)
+STOCKS = itertools.product(
+    ['SPY', 'QQQ', 'TSLA'],
+    ['2010-01-01', '2015-01-31',],
+    ['2023-01-01', '2023-01-31',],
+)
 
-@pytest.mark.parametrize('symbol', ['BTC-USD', 'ETH-USD', 'BNB-USD'])
-@pytest.mark.parametrize('freq', ['1d', ])
-@pytest.mark.parametrize('start', ['2022-01-01', '2022-01-31',])
-@pytest.mark.parametrize('end', ['2023-01-01', '2023-01-31',])
-def test_fetch_sqlite(symbol, freq, start, end):
+
+# @pytest.mark.parametrize('symbol', ['BTC-USD', 'ETH-USD', 'BNB-USD'])
+# @pytest.mark.parametrize('start', ['2022-01-01', '2022-01-31',])
+# @pytest.mark.parametrize('end', ['2023-01-01', '2023-01-31',])
+@pytest.mark.parametrize('symbol, start, end', [*CRYPTOS, *STOCKS])
+def test_fetch_sqlite(symbol, start, end):
     async def main():
-        df = await fetch_sqlite(symbol, freq, start, end)
+        df = await fetch_sqlite(symbol, FREQ, start, end)
         assert isinstance(df, DataFrame)
         assert df.shape[1] == 5
         assert df.index.is_monotonic_increasing
         assert df.index[0] >= to_datetime(start)
         assert df.index[-1] <= to_datetime(end)
-        for open, high, low, close, *_ in df.itertuples(index=False):
+        for row in df.itertuples(index=False):
+            open, high, low, close, *_ = tuple(map(lambda x: round(x, 4), row))
             assert low <= open <= high
             assert low <= close <= high
 
