bef68a9d45c878dcd736c0c31c00d1237ed9268a
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 20:10:41 +0800

    rft: adopting sqlite cache

--- trbox/market/yahoo/historical/windows/__init__.py
+++ trbox/market/yahoo/historical/windows/__init__.py
@@ -1,26 +1,25 @@
+import asyncio
 from threading import Event
 
-import yfinance as yf
-from pandas import DataFrame, Timestamp, to_datetime
+from pandas import DataFrame, Timedelta, Timestamp, to_datetime
 from typing_extensions import override
 
-from trbox.common.constants import OHLCV_COLUMN_NAMES
-from trbox.common.types import Symbols
-from trbox.common.utils import trim_ohlcv_by_range_length
+from trbox.common.types import Symbol, Symbols
 from trbox.event.broker import AuditRequest
 from trbox.event.market import OhlcvWindow
 from trbox.market import MarketWorker
 from trbox.market.utils import make_combined_rolling_windows
+from trbox.market.yahoo.historical.windows.constants import Freq
+from trbox.market.yahoo.historical.windows.use_dl_sqlite_cache import \
+    fetch_sqlite
 
 
-def yfinance_download(symbol: str, interval: str) -> DataFrame:
-    ticker = yf.Ticker(symbol)
-    df: DataFrame = ticker.history(period='max',
-                                   interval=interval)
-    df = DataFrame(df.tz_localize(None))
-    df = df[OHLCV_COLUMN_NAMES]
-    print(f'downloaded ohlcv, symbol="{symbol}", shape={df.shape}', flush=True)
-    return df
+def cal_win_start(start: Timestamp, freq: Freq, length: int) -> Timestamp:
+    num = int(freq[:-1])*length
+    if freq.endswith('d'):
+        return start - Timedelta(days=num)
+    else:
+        raise ValueError
 
 
 class YahooHistoricalWindows(MarketWorker):
@@ -28,28 +27,32 @@ class YahooHistoricalWindows(MarketWorker):
                  symbols: Symbols,
                  start: Timestamp | str,
                  end: Timestamp | str | None = None,
-                 interval: str = '1d',
+                 freq: Freq = '1d',
                  length: int) -> None:
         super().__init__()
         self._symbols = symbols
         self._start = to_datetime(start)
-        self._end = to_datetime(end)
-        self._interval = interval
+        self._end = to_datetime(end if end else Timestamp.now())
+        self._freq = freq
         self._length = length
         # data preprocessing
-        self._dfs = {s: yfinance_download(
-            s, self._interval) for s in self._symbols}
-        # data validation
-        self._dfs = {s: trim_ohlcv_by_range_length(df, self._start, self._end, self._length)
-                     for s, df in self._dfs.items()}
-        # data ready
-        self._window_generator = make_combined_rolling_windows(
-            self._dfs, self._length)
+        self._win_start = cal_win_start(self._start, self._freq, self._length)
+        self._dfs_coros = {s: fetch_sqlite(s, self._freq, self._win_start, self._end)
+                           for s in self._symbols}
         # work thread event
         self._alive = Event()
 
     @override
     def working(self) -> None:
+        # gather dfs
+        async def gather_dfs() -> dict[Symbol, DataFrame]:
+            results = await asyncio.gather(*self._dfs_coros.values())
+            return dict(zip(self._symbols, results))
+        self._dfs = asyncio.run(gather_dfs())
+        # data ready
+        self._window_generator = make_combined_rolling_windows(
+            self._dfs, self._length)
+        # start generator
         for symbol, df in self._window_generator:
             hb = self.strategy.heartbeats.get(
                 (symbol, OhlcvWindow), None)
