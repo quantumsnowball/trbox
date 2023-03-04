1ba7b8294007a18abd5b42a618a94b40a774f05c
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-04 13:46:23 +0800

    feat: retry loop

--- trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
+++ trbox/market/yahoo/historical/windows/use_dl_sqlite_cache.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 import aiohttp
 import aiosqlite
-from pandas import (DataFrame, Timestamp, date_range, read_csv,
-                    to_datetime)
+from pandas import DataFrame, Timestamp, date_range, read_csv, to_datetime
 from socketio.asyncio_client import asyncio
 
 from trbox.common.constants import OHLCV_INDEX_NAME
+from trbox.common.logger import Log
 from trbox.market.yahoo.historical.windows.constants import CACHE_DIR, Freq
 
 
@@ -45,28 +45,38 @@ async def fetch_sqlite(symbol: str,
             async with aiohttp.ClientSession() as session:
                 missing_start = int(min(missing))
                 missing_end = int(max(missing)) + 60*60*24
-
-                async def download() -> DataFrame:
-                    base = 'https://query1.finance.yahoo.com/v7/finance/download'
-                    other_params = 'interval=1d&events=history&includeAdjustedClose=true'
-                    download_url = f'{base}/{symbol}?period1={missing_start}&period2={missing_end}&{other_params}'
-                    async with session.get(download_url) as res:
-                        assert res.status == 200
-                        csv = await res.content.read()
-                        df = read_csv(BytesIO(csv))
-                        df = df.set_index('Date')
-                        df.index = to_datetime(df.index.values)
-                        df = df.drop('Close', axis=1)
-                        df = df.rename(columns={'Adj Close': 'Close'})
-                        return df
-                downloaded = await download()
-                print(f'downloaded ohlcv, symbol="{symbol}", shape={downloaded.shape}',
-                      flush=True)
-                downloaded_tuples = list(downloaded.itertuples(index=True))
-                entries = [tuple((int(t[0].timestamp()), *t[1:]))
-                           for t in downloaded_tuples]
-                await db.executemany('REPLACE INTO ohlcv VALUES(?, ?, ?, ?, ?, ?)', entries)
-                await db.commit()
+                for _ in range(retry):
+                    try:
+                        # download
+                        async def download() -> DataFrame:
+                            base = 'https://query1.finance.yahoo.com/v7/finance/download'
+                            other_params = 'interval=1d&events=history&includeAdjustedClose=true'
+                            download_url = f'{base}/{symbol}?period1={missing_start}&period2={missing_end}&{other_params}'
+                            async with session.get(download_url) as res:
+                                assert res.status == 200
+                                csv = await res.content.read()
+                                df = read_csv(BytesIO(csv))
+                                df = df.set_index('Date')
+                                df.index = to_datetime(df.index.values)
+                                df = df.drop('Close', axis=1)
+                                df = df.rename(columns={'Adj Close': 'Close'})
+                                return df
+                        downloaded = await download()
+                        print(f'downloaded ohlcv, symbol="{symbol}", shape={downloaded.shape}',
+                              flush=True)
+                        # extract
+                        downloaded_tuples = list(
+                            downloaded.itertuples(index=True))
+                        entries = [tuple((int(t[0].timestamp()), *t[1:]))
+                                   for t in downloaded_tuples]
+                        # insert
+                        await db.executemany('REPLACE INTO ohlcv VALUES(?, ?, ?, ?, ?, ?)', entries)
+                        await db.commit()
+                        # successfully done
+                        break
+                    except Exception as e:
+                        # will retry download and insert on Exception
+                        Log.exception(e)
         # read the requested data
         sql_select = ('SELECT Timestamp,Open,High,Low,Close,Volume '
                       'FROM ohlcv WHERE '
