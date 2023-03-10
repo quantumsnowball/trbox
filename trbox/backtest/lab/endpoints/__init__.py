cd97c3f9588ad9db3a79bf757c759012f4340b09
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-10 18:51:23 +0800

    lint: fix

--- trbox/backtest/lab/endpoints/__init__.py
+++ trbox/backtest/lab/endpoints/__init__.py
@@ -2,3 +2,10 @@ from trbox.backtest.lab.endpoints.operation import routes as operation
 from trbox.backtest.lab.endpoints.result import routes as result
 from trbox.backtest.lab.endpoints.source import routes as source
 from trbox.backtest.lab.endpoints.static import routes as static
+
+__all__ = [
+    'operation',
+    'result',
+    'source',
+    'static',
+]
