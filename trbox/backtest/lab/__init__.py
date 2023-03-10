1392c127dea812815dd095143ab169a8a6c0b08c
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-10 19:08:44 +0800

    rft: tree

--- trbox/backtest/lab/__init__.py
+++ trbox/backtest/lab/__init__.py
@@ -1,56 +1,17 @@
 import asyncio
-import os
 from asyncio import Future
-from pathlib import Path
 from threading import Thread
 from typing import Any
 
 import click
 from aiohttp import web
 from aiohttp.typedefs import Handler
-from binance.websocket.binance_socket_manager import json
 from typing_extensions import override
 
 import trbox.backtest.lab.endpoints as endpoints
-from trbox.backtest.lab.constants import ENTRY_POINT, PY_SUFFIX, RUNDIR_PREFIX
-from trbox.backtest.utils import Node
+from trbox.backtest.lab.constants import ENTRY_POINT
 from trbox.common.logger import Log
 from trbox.common.logger.parser import Memo
-
-
-def scan_for_source(parent: Node,
-                    *,
-                    suffix: str = PY_SUFFIX,
-                    prefix_excluded: str = RUNDIR_PREFIX,
-                    basepath: str) -> Node:
-    for m in os.scandir(basepath + parent.path):
-        if m.is_dir() and not m.name.startswith(prefix_excluded):
-            parent.add(scan_for_source(Node(m.name, 'folder', parent, []),
-                                       basepath=basepath))
-        elif m.is_file() and m.name.endswith(suffix):
-            parent.add(Node(m.name, 'file', parent, []))
-    return parent
-
-
-def scan_for_result(parent: Node,
-                    *,
-                    prefix: str = RUNDIR_PREFIX,
-                    basepath: str) -> Node:
-    # loop through every items in path
-    for m in os.scandir(basepath + parent.path):
-        if m.is_dir():
-            if m.name.startswith(prefix):
-                # prefixed dir should contain run info
-                db_path = f'{basepath}/{parent.path}/{m.name}/db.sqlite'
-                if (Path(db_path).is_file()):
-                    # meta.json should exist in a valid result dir
-                    parent.add(scan_for_result(Node(m.name, 'folder', parent, []),
-                                               basepath=basepath))
-            else:
-                # nested one level if is a dir, key is the dirname
-                parent.add(scan_for_result(Node(m.name, 'folder', parent, []),
-                                           basepath=basepath))
-    return parent
 
 
 class Lab(Thread):
@@ -68,10 +29,7 @@ class Lab(Thread):
         self._port = port
         self._app = web.Application(middlewares=[self.on_request_error, ])
         # match api first
-        self._app.add_routes([
-            web.get('/api/tree/source', self.ls_source),
-            web.get('/api/tree/result', self.ls_result),
-        ])
+        self._app.add_routes(endpoints.tree(self._path))
         self._app.add_routes(endpoints.source)
         self._app.add_routes(endpoints.result)
         self._app.add_routes(endpoints.operation)
@@ -79,21 +37,6 @@ class Lab(Thread):
         self._app.add_routes(endpoints.static)
         # app
         self._runner = web.AppRunner(self._app)
-
-    #
-    # routes
-    #
-    async def ls_source(self, _: web.Request) -> web.Response:
-        node = scan_for_source(Node('', 'folder', None, []),
-                               basepath=self._path)
-        return web.json_response(node.dict,
-                                 dumps=lambda s: str(json.dumps(s, indent=4)))
-
-    async def ls_result(self, _: web.Request) -> web.Response:
-        node = scan_for_result(Node('', 'folder', None, []),
-                               basepath=self._path)
-        return web.json_response(node.dict,
-                                 dumps=lambda s: str(json.dumps(s, indent=4)))
 
     #
     # error handling
