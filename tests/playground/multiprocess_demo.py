071017cb697885b4b3dff80f60f707d44138349c
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-16 19:38:57 +0800

    demo: how to pull out the monitor on its own thread/process

--- tests/playground/multiprocess_demo.py
+++ tests/playground/multiprocess_demo.py
@@ -1,40 +1,71 @@
+from abc import ABC, abstractmethod
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
-from multiprocessing import Process
-from queue import Queue
+from multiprocessing import Process, Queue
+# from queue import Queue
 from time import sleep
 
 
-class Handler:
+class Handler(ABC):
     def __init__(self):
         self.queue = Queue()  # this object is essential, but not picklable
 
     def put(self, item):
         self.queue.put(item)
 
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class Monitor(Handler):
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+
     def run(self):
         while True:
-            sleep(1)
+            print('getting progress...')
+            item = self.queue.get()
+            print('received an item')
+            if item == 'exit':
+                break
+            self.count += 1
+            print(f'msg count = {self.count}')
+
+
+monitor = Monitor()
+
+
+class Worker(Handler):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        while True:
             item = self.queue.get()
             if item == 'exit':
                 break
-            # do other things on the item ...
+            # do other things on the item, may be cpu intensive ...
             print(item)
+            monitor.put('count me')
 
 
 class Runner:
     def __init__(self, name):
         self.name = name
-        self.a = Handler()
-        self.b = Handler()
+        self.a = Worker()
+        self.b = Worker()
 
     def start(self):
         for i in range(4):
             self.a.put(f'{self.name}: hello a ({i})')
             self.b.put(f'{self.name}: hello b ({i})')
-        self.a.put('exit')
-        self.b.put('exit')
+        # self.a.put('exit')
+        # self.b.put('exit')
+        # monitor.put('exit')
         with ThreadPoolExecutor() as exe:
-            futures = [exe.submit(r.run) for r in [self.a, self.b]]
+            futures = [exe.submit(r.run)
+                       for r in [self.a, self.b]]
         for future in futures:
             future.result()
 
@@ -65,7 +96,8 @@ def run_in_process():
     rA = Runner('A')
     rB = Runner('B')
     rC = Runner('C')
-    procs = [Process(target=r.start) for r in [rA, rB, rC]]
+    procs = [*[Process(target=r.start)
+             for r in [rA, rB, rC]], Process(target=monitor.run)]
     for p in procs:
         p.start()
     for p in procs:
