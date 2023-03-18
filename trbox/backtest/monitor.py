862b60b501cfaee4aa4250904a10f3a37c7bc631
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-18 19:50:25 +0800

    step: Tracker, send to queue only on step

--- trbox/backtest/monitor.py
+++ trbox/backtest/monitor.py
@@ -2,6 +2,8 @@ from collections import deque
 from multiprocessing import Queue
 from queue import Full
 
+from pandas import Timestamp
+
 from trbox.common.utils import localnow_string
 from trbox.event import Event
 from trbox.event.monitor import EnableOutput, ProgressUpdate
@@ -39,11 +41,8 @@ class Monitor:
     # handle events
 
     def handle_progress_update(self, e: ProgressUpdate) -> None:
-        # calculate overall progress
-        total = e.end - e.start
-        done = e.current - e.start
-        pct = max(0, min(1, done / total))
-        self._pcts[e.name] = pct
+        # update overall progress
+        self._pcts[e.name] = e.pct
         # only when display is enabled
         if self._enable:
             # calculate the latest overall progress
@@ -64,3 +63,30 @@ class Monitor:
 
 # singleton instance for all traders to share
 monitor = Monitor()
+
+
+#
+# helper
+#
+class Tracker:
+    def __init__(self,
+                 start: Timestamp,
+                 end: Timestamp,
+                 step: int = 5) -> None:
+        self._start = start
+        self._end = end
+        self._step = step
+        self._rolling: deque[float] = deque([0, 0], maxlen=2)
+        self.total = self._end - self._start
+
+    def update(self,
+               name: str,
+               current: Timestamp,):
+        # calc current progress
+        done = current - self._start
+        pct = max(0, min(1, done / self.total))
+        self._rolling.append(pct)
+        # check if overall progress is crossing a mark
+        prev, val = self._rolling
+        if int(prev*100//self._step) != int(val*100//self._step):
+            monitor.put(ProgressUpdate(name, pct))
