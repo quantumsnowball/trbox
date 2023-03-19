c1eb091157dc87d1267e116ec157374bd697fa76
Quantum Snowball <quantum.snowball@gmail.com>
2023-03-19 11:06:50 +0800

    playground: Process

--- playground/multiprocess_demo.py
+++ playground/multiprocess_demo.py
@@ -1,88 +1,49 @@
-from abc import ABC, abstractmethod
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
-from multiprocessing import Process, Queue
-# from queue import Queue
+from multiprocessing import Process
+from queue import Queue
 from time import sleep
 
+# from multiprocessing import Queue
 
-class Handler(ABC):
+
+class Handler:
     def __init__(self):
-        self.queue = Queue()  # this object is essential, but not picklable
+        self.queue = Queue()  # this object is essential
 
     def put(self, item):
         self.queue.put(item)
 
-    @abstractmethod
-    def run(self):
-        pass
-
-
-class Monitor(Handler):
-    def __init__(self):
-        super().__init__()
-        self.count = 0
-
     def run(self):
         while True:
             item = self.queue.get()
             if item == 'exit':
                 break
-            self.count += 1
-            # print(f'msg count = {self.count}')
-
-
-monitor = Monitor()
-
-
-class Worker(Handler):
-    def __init__(self):
-        super().__init__()
-        self.last_msg = 'init'
-
-    def run(self):
-        while True:
-            item = self.queue.get()
-            self.last_msg = item
-            if item == 'exit':
-                break
-            # do other things on the item, may be cpu intensive ...
-            # print(item)
-            monitor.put('count me')
+            # do other things on the item ...
+            print(item)
+            sleep(1)
 
 
 class Runner:
     def __init__(self, name):
         self.name = name
-        self.a = Worker()
-        self.b = Worker()
+        self.a = Handler()
+        self.b = Handler()
 
     def start(self):
-        for i in range(4):
-            self.a.put(f'{self.name}: hello a ({i})')
-            self.b.put(f'{self.name}: hello b ({i})')
+        # some dummy messages
+        for _ in range(3):
+            self.a.put(f'{self.name}: hello a')
+            self.b.put(f'{self.name}: hello b')
         self.a.put('exit')
         self.b.put('exit')
-        monitor.put('exit')
         with ThreadPoolExecutor() as exe:
-            futures = [exe.submit(r.run)
-                       for r in [self.a, self.b]]
-        for future in futures:
-            future.result()
+            futures = [exe.submit(r.run) for r in [self.a, self.b]]
+            for f in futures:
+                f.result()
 
 
-# current implementation
-def run_in_multi_thread():
-    rA = Runner('A')
-    rB = Runner('B')
-    rC = Runner('C')
-    with ThreadPoolExecutor() as exe:
-        futures = [exe.submit(r.start) for r in [rA, rB, rC]]
-        for future in futures:
-            future.result()
-
-
-# how to implement this?
-def run_in_multi_process_pool():
+# this requires everything to be picklable
+def run_in_process_pool():
     rA = Runner('A')
     rB = Runner('B')
     rC = Runner('C')
@@ -92,21 +53,18 @@ def run_in_multi_process_pool():
             future.result()
 
 
-def run_in_process():
+# this does not pickle anythin, but why?
+def run_in_processes():
     rA = Runner('A')
     rB = Runner('B')
     rC = Runner('C')
-    print(rA.a.last_msg)
-    procs = [*[Process(target=r.start)
-             for r in [rA, rB, rC]], Process(target=monitor.run)]
+    procs = [Process(target=r.start) for r in [rA, rB, rC]]
     for p in procs:
         p.start()
     for p in procs:
         p.join()
-    print(rA.a.last_msg)
 
 
 if __name__ == '__main__':
-    # run_in_multi_thread()  # this is currently running fine
-    # run_in_multi_process_pool()  # this doesn't work, need to make everything picklable
-    run_in_process()
+    # run_in_process_pool()  # `TypeError: cannot pickle '_thread.lock' object`
+    run_in_processes()  # this is working
